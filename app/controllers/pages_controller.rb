aec6a19d61ac75d2727ee4a453b50a2102aae09d
eldiestrogato <andy.parkour.2119@gmail.com>
2022-06-13 18:53:01 +0200

    adding bootstrap

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def about
     @heading = 'Page about Us!'
-    @textabout = 'Just some text about us, bruh'
+    @textabout = 'Site is just for tests'
   end
 end
